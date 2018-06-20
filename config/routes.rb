5f8a6242e06db3176f9fc88ce33913e041cd70c5
Elliott Potter <elliott@shipt.com>
2018-06-19 23:00:13 -0500

    Add manifest files and root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :advertisers
   resources :hosts
   post 'events/create' => 'events#create'
+  root to: "hosts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
